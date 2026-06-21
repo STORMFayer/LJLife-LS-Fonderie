1e493c8848ccacddb87787f86257ce125b1ce51c
Rafael <r.iglesiaslopez@student.helmo.be>
2026-06-21 14:47:33 +0200

    modifi

--- script.js
+++ script.js
@@ -15,7 +15,62 @@ function sendData() {
     document.getElementById("msg").innerText = "Ajouté avec succès !";
   });
 }
+function login() {
+  const pass = document.getElementById("pass").value;
 
+  if (pass === "Zeubi") {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("adminPanel").style.display = "block";
+    loadAdmin();
+  } else {
+    alert("Mot de passe incorrect");
+  }
+}
+function loadEmploye() {
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+
+      const nom = localStorage.getItem("nom");
+      const prenom = localStorage.getItem("prenom");
+      const discord = localStorage.getItem("discord");
+
+      let total = 0;
+
+      data.forEach(e => {
+        if (e.nom === nom && e.prenom === prenom && e.discord === discord) {
+          total += Number(e.minerais);
+        }
+      });
+
+      document.getElementById("total").innerText =
+        "Total minerais : " + total;
+    });
+}
+function classement(data) {
+  let stats = {};
+
+  data.forEach(e => {
+    let key = e.nom + " " + e.prenom;
+
+    if (!stats[key]) stats[key] = 0;
+
+    stats[key] += Number(e.minerais);
+  });
+
+  let sorted = Object.entries(stats)
+    .sort((a, b) => b[1] - a[1]);
+
+  let html = "<h3>Classement Mineurs</h3>";
+
+  sorted.forEach((e, i) => {
+    html += `
+      <p>#${i+1} ${e[0]} - ${e[1]} minerais</p>
+    `;
+  });
+
+  document.getElementById("ranking").innerHTML = html;
+}
 // ADMIN LOAD
 if (window.location.pathname.includes("admin")) {
   fetch(API_URL)
